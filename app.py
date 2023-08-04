cc40841fc07096618286df187db100c7a86e29e1
kimmothy <kimmothy@naver.com>
2023-08-04 15:50:31 +0900

    openai

--- app.py
+++ app.py
@@ -18,15 +18,52 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+def getAOAIResponse(message):
+    try: 
+
+        # Get configuration settings 
+        load_dotenv()
+        azure_oai_endpoint = os.getenv("AZURE_OAI_ENDPOINT")
+        azure_oai_key = os.getenv("AZURE_OAI_KEY")
+        azure_oai_model = os.getenv("AZURE_OAI_MODEL")
+        
+        # Read text from file
+        text = message
+        
+        # Set OpenAI configuration settings
+        openai.api_type = "azure"
+        openai.api_base = azure_oai_endpoint
+        openai.api_version = "2023-03-15-preview"
+        openai.api_key = azure_oai_key
+
+        # Send request to Azure OpenAI model
+        print("Sending request for summary to Azure OpenAI endpoint...\n\n")
+        response = openai.ChatCompletion.create(
+            engine=azure_oai_model,
+            temperature=0.7,
+            max_tokens=120,
+            messages=[
+            {"role": "system", "content": "You are a freindly AI. Answer my questions kindly."},
+                {"role": "user", "content": text}
+            ]
+        )
+
+        return response
+        
+
+    except Exception as ex:
+        print(ex)
+
 @app.route('/hello', methods=['POST'])
 def hello():
-   name = request.form.get('name')
+   message = request.form.get('message')
 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+   if message:
+       print('Request for hello page received with message=%s' % message)
+       response = getAOAIResponse(message)["choices"][0]["message"]["content"]
+       return render_template('hello.html', response = response)
    else:
-       print('Request for hello page received with no name or blank name -- redirecting')
+       print('Request for hello page received with no message or blank message -- redirecting')
        return redirect(url_for('index'))
 
 
